9aaf0d3713e6f2159907ceff09a18b5cc86ab00d
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-29 18:05:44 +0100

    Header Icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
 import styled from "styled-components";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+    </Container>
+  );
 }
 
 export default Sidebar;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 10px;
+`;
+
+const IconsContainer = styled.div``;
