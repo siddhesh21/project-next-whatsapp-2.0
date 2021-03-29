1d70c3a790d508f5ab7e28eb7b2a6715e5db0bee
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-29 12:20:49 +0100

    Added initial files

---
+++ components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Sidebar() {
+  return <Container></Container>;
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
